4fcd4b0bc59a21e32c74a27a2577225fd5db9de9
ortegaj716 <ortegaj716@gmail.com>
2014-04-25 13:58:53 -0400

    Some Mongoose, Needs More

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,41 @@ exports.index = function(req, res){
   res.render('index', { title: 'Express' });
 };
 
-//This will be changed to index later, but for now, let's keep it separate.
-exports.mongo = function(req, res{
+//This will be called by Ajax
+exports.mongo = function(req, res){
 
-}
+	var mongoose = require('../node_modules/mongoose');
+	mongoose.connect('mongodb://localhost/facebookDB');
+
+	var db = mongoose.connection;
+	db.on('error', console.error.bind(console, 'connection error:'));
+	db.once('open', function callback(){
+
+		console.log("Connected!");
+		
+		//Let's get started
+		//Our Schema
+		var Schema = mongoose.Schema;
+		var facebookSchema = new Schema({
+		
+			Country: String,
+			TotalRequests: Number,
+			AccountsRequested: Number,
+			Percent: Number
+		});
+		
+		//Our Model
+		var DataModel = mongoose.model('DataModel',facebookSchema,'fbData');
+
+		//Let's try printing something...
+		DataModel.find(function(err, results){
+			if(err) return console.error(err);
+			console.log(results);
+		});
+
+	});
+
+//Just for now, browser needs something to receive
+res.send("OK");
+
+};
