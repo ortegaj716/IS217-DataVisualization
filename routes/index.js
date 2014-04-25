a4e44766fe0c753b2c5d414002af466eb877d5ef
ortegaj716 <ortegaj716@gmail.com>
2014-04-25 13:05:09 -0400

    Preparing for Mongoose

--- routes/index.js
+++ routes/index.js
@@ -6,3 +6,8 @@
 exports.index = function(req, res){
   res.render('index', { title: 'Express' });
 };
+
+//This will be changed to index later, but for now, let's keep it separate.
+exports.mongo = function(req, res{
+
+}
