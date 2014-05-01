55bb212ec793d31bc8c5931faeb2be1572755bdf
bindi123 <bindirana6@gmail.com>
2014-05-01 08:24:45 -0400

    bar chart code need to fix some error

--- public/barCode.js
+++ public/barCode.js
@@ -1,64 +1,80 @@
-//Fun with jQuery!
+function renderChart() {
 
-var mapData = {};
-var dm = {};
+var data = d3.csv.parse(d3.select('#csv').text());
+var valueLabelWidth = 40; // space reserved for value labels (right)
+var barHeight = 20; // height of one bar
+var barLabelWidth = 100; // space reserved for bar labels
+var barLabelPadding = 5; // padding between bar and bar labels (left)
+var gridLabelHeight = 18; // space reserved for gridline labels
+var gridChartOffset = 3; // space between start of grid and first bar
+var maxBarWidth = 420; // width of the bar with the max value
+ 
+// accessor functions 
+var barLabel = function(d) { return d['Country']; };
+var barValue = function(d) { return parseFloat(d['TotalRequests']); };
+ 
+// sorting
+var sortedData = data.sort(function(a, b) {
+ return d3.ascending(barLabel(a), barLabel(b));
+}); 
 
-$(document).ready(function(){
+// scales
+var yScale = d3.scale.ordinal().domain(d3.range(0, sortedData.length)).rangeBands([0, sortedData.length * barHeight]);
+var y = function(d, i) { return yScale(i); };
+var yText = function(d, i) { return y(d, i) + yScale.rangeBand() / 2; };
+var x = d3.scale.linear().domain([0, d3.max(sortedData, barValue)]).range([0, maxBarWidth]);
+// svg container element
+var chart = d3.select('#chart').append("svg")
+  .attr('width', maxBarWidth + barLabelWidth + valueLabelWidth)
+  .attr('height', gridLabelHeight + gridChartOffset + sortedData.length * barHeight);
+// grid line labels
+var gridContainer = chart.append('g')
+  .attr('transform', 'translate(' + barLabelWidth + ',' + gridLabelHeight + ')'); 
+gridContainer.selectAll("text").data(x.ticks(10)).enter().append("text")
+  .attr("x", x)
+  .attr("dy", -3)
+  .attr("text-anchor", "middle")
+  .text(String);
+// vertical grid lines
+gridContainer.selectAll("line").data(x.ticks(10)).enter().append("line")
+  .attr("x1", x)
+  .attr("x2", x)
+  .attr("y1", 0)
+  .attr("y2", yScale.rangeExtent()[1] + gridChartOffset)
+  .style("stroke", "#ccc");
+// bar labels
+var labelsContainer = chart.append('g')
+  .attr('transform', 'translate(' + (barLabelWidth - barLabelPadding) + ',' + (gridLabelHeight + gridChartOffset) + ')'); 
+labelsContainer.selectAll('text').data(sortedData).enter().append('text')
+  .attr('y', yText)
+  .attr('stroke', 'none')
+  .attr('fill', 'black')
+  .attr("dy", ".35em") // vertical-align: middle
+  .attr('text-anchor', 'end')
+  .text(barLabel);
+// bars
+var barsContainer = chart.append('g')
+  .attr('transform', 'translate(' + barLabelWidth + ',' + (gridLabelHeight + gridChartOffset) + ')'); 
+barsContainer.selectAll("rect").data(sortedData).enter().append("rect")
+  .attr('y', y)
+  .attr('height', yScale.rangeBand())
+  .attr('width', function(d) { return x(barValue(d)); })
+  .attr('stroke', 'white')
+  .attr('fill', 'steelblue');
+// bar value labels
+barsContainer.selectAll("text").data(sortedData).enter().append("text")
+  .attr("x", function(d) { return x(barValue(d)); })
+  .attr("y", yText)
+  .attr("dx", 3) // padding-left
+  .attr("dy", ".35em") // vertical-align: middle
+  .attr("text-anchor", "start") // text-align: right
+  .attr("fill", "black")
+  .attr("stroke", "none")
+  .text(function(d) { return d3.round(barValue(d), 2); });
+// start line
+barsContainer.append("line")
+  .attr("y1", -gridChartOffset)
+  .attr("y2", yScale.rangeExtent()[1] + gridChartOffset)
+  .style("stroke", "#000");
 
-	$.getJSON("http://localhost:3000/mongo", function(data) {
-	/*
-		var items = [];
-		$.each(data, function(key,val){
-			items.push("<li id='" + key + "'>" + val + "</li>" );
-		});
-
-		$("<ul/>", {
-			"class": "myNewList",
-			html: items.join("")
-		}).appendTo("body");
-	*/
-
-		var mapData = data;
-
-		console.log("OK");
-
-		console.log(mapData);
-
-		dm = {
-			element: document.getElementById('container'),
-	
-			fills: {
-				defaultFill: 'rgb(240,240,240)',
-				'low': 'rgb(199,233,255)',
-				'some': 'rgb(137,207,250)',
-				'high': 'rgb(0,156,255)',
-				'veryHigh': 'rgb(0,112,181)'
-			},
-	
-			data: mapData[0],
-	
-			geographyConfig: {
-	
-				popupTemplate: function(geo, data) {
-					//console.log(data);
-					//console.log(mapData);
-					
-					if(data == null)
-						return '<div class="hoverinfo"><strong>' + geo.properties.name + '</strong></div>';
-
-					//Wow look at this mess
-
-					return '<div class="hoverinfo"><strong>' + geo.properties.name + '</strong><ul><li>Total Requests: ' + data.TotalRequests + '</li><li>User Accounts Requested: ' + data.AccountsRequested + '</li><li>Percent of Requests Fulfilled: ' + data.Percent + '</li></ul></div>';
-
-					//return '<div class="hoverinfo"><strong>' + geo.properties.name + ' ' + ' ' + data.Percent + '</strong></div>';
-				}
-				
-			}
-	
-		};
-
-		$("#container").datamaps(dm);
-
-	});
-
-});
+}
